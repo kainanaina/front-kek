3f70d7e4f6c14f6fa742410b82568bd87183c742
kainanaina <kainanaina@yandex.ru>
2023-07-23 23:58:09 +0800

    fix codepen embed issue

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const GITHUB_REPO = 'https://github.com/kainanaina/front-kek';
 
 export const TWITTER = 'https://twitter.com/NikolayTalanov';
 
-export const CODEPEN_PROFILE = 'https://codepen.io/suez/pen';
+export const CODEPEN_PROFILE = 'https://codepen.io/suez';
 
 export const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/nikolaytalanov/';
 
